98ed5a391bcef0febcfa080aa0f8526c78a1063d
kielsonzinn <kielsonzinn@gmail.com>
2024-10-21 22:37:42 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-# acr-extension-name
+# acr-cpp-param-name
 
 Arquivo config.json
 
 ```json
 {
-
+  "stage": "static",
+  "rules": [
+    {
+      "message": "Parametro não pode iniciar com o prefixo new, verifique o parametro ${PARAM_NAME}.<br>Arquivo ${FILE_PATH}<br>Linha: ${LINE_NUMBER}",
+      "regex": "new.*"
+    },
+    {
+      "message": "Parametro não pode iniciar com a primeira letra maiscula, verifique o parametro ${PARAM_NAME}.<br>Arquivo ${FILE_PATH}<br>Linha: ${LINE_NUMBER}",
+      "regex": "[A-Z].*"
+    }
+  ]
 }
 ```
--- src/review.py
+++ src/review.py
@@ -1,25 +1,116 @@
+import json
+import re
+import subprocess
+
 import automatic_code_review_commons as commons
 
-def review(config):
-    path_target = config['path_target']
-    path_source = config['path_source']
 
-    merge = config['merge']
-    project_id = merge['project_id']
-    merge_request_id = merge['merge_request_id']
-    
+def review(config):
+    path_source = config['path_source']
+    changes = config['merge']['changes']
+    rules = config['rules']
     comments = []
-    
-    # TODO IMPLEMENTAR EXTENSION
-    #  O OBJETO DE COMENTARIO DEVE POSSUIR O SEGUINTE FORMATO
-    #  commons.comment_create(
-    #     comment_id=commons.comment_generate_id( "" ),
-    #     comment_path="",
-    #     comment_description="",
-    #     comment_snipset=True,
-    #     comment_end_line=1,
-    #     comment_start_line=1,
-    #     comment_language="",
-    # )
+
+    for change in changes:
+        new_path = change['new_path']
+        path = path_source + "/" + new_path
+
+        if not path.endswith(('.h', '.cpp')):
+            continue
+
+        objs = get_cpp_methods_json(path)
+
+        for obj in objs:
+            if 'pattern' not in obj:
+                continue
+
+            if obj['kind'] == 'property':
+                continue
+
+            params = extract_parameters_from_signature(obj['pattern'])
+
+            for param in params:
+                if param.strip() == "":
+                    continue
+
+                if "=" in param:
+                    param = param[0:param.index('=')].strip()
+
+                parts = param.split(" ")
+                param_name = parts[len(parts) - 1]
+
+                if param_name.endswith("&"):
+                    continue
+
+                for obj_regex in rules:
+                    regex = obj_regex['regex']
+
+                    if re.match(regex, param_name):
+                        line_number = obj['line']
+                        comment_description = obj_regex['message']
+                        comment_description = comment_description.replace("${PARAM_NAME}", param_name)
+                        comment_description = comment_description.replace("${FILE_PATH}", new_path)
+                        comment_description = comment_description.replace("${LINE_NUMBER}", str(line_number))
+
+                        comments.append(commons.comment_create(
+                            comment_id=commons.comment_generate_id(comment_description),
+                            comment_path=new_path,
+                            comment_description=comment_description,
+                            comment_snipset=True,
+                            comment_end_line=line_number,
+                            comment_start_line=line_number,
+                            comment_language='c++',
+                        ))
 
     return comments
+
+
+def remove_templates(param):
+    while re.search(r'<[^<>]*>', param):
+        param = re.sub(r'<[^<>]*>', '', param)
+    return param
+
+
+def extract_parameters_from_signature(signature):
+    if "Q_ENUM" in signature:
+        return []
+
+    if "(" not in signature:
+        return []
+
+    part = signature[signature.rindex('(') + 1:]
+
+    if ")" not in part:
+        return []
+
+    part = part[0:part.rindex(')')]
+    part = part.strip()
+
+    part = remove_templates(part)
+
+    params = part.split(",")
+
+    if "" in params:
+        params.remove("")
+
+    return params
+
+
+def get_cpp_methods_json(file_name):
+    cmd = ['ctags', '--output-format=json', '--fields=+n', '--c++-kinds=f', '--c++-kinds=+p', file_name]
+
+    result = subprocess.run(cmd, capture_output=True, text=True)
+
+    if result.returncode != 0:
+        raise Exception("ctags command error")
+
+    retorno = result.stdout.split("\n")
+
+    objs = []
+    for obj in retorno:
+        if obj == "":
+            continue
+
+        objs.append(json.loads(obj))
+
+    return objs
